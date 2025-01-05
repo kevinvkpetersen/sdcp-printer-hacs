05fdfb61ca05ca290dfe97d53f999992686d8d08
Kevin Petersen <kevinvkpetersen@gmail.com>
2025-01-05 02:14:36 -0800

    Enum rename

--- custom_components/sdcp_printer/sensor.py
+++ custom_components/sdcp_printer/sensor.py
@@ -5,7 +5,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from sdcp_printer.enum import SDCPStatus
+from sdcp_printer.enum import SDCPMachineStatus
 
 from .const import DOMAIN
 from .coordinator import SDCPPrinterDataUpdateCoordinator
@@ -30,19 +30,21 @@ class SDCPPrinterCurrentStatusSensor(BaseSDCPPrinterEntity, SensorEntity):
     @property
     def native_value(self):
         """Return the state of the sensor."""
-        current_status: list[SDCPStatus] = self.coordinator.printer.current_status
+        current_status: list[SDCPMachineStatus] = (
+            self.coordinator.printer.current_status
+        )
         if not current_status:
             return None
 
-        if SDCPStatus.IDLE in current_status:
+        if SDCPMachineStatus.IDLE in current_status:
             return "Idle"
-        if SDCPStatus.PRINTING in current_status:
+        if SDCPMachineStatus.PRINTING in current_status:
             return "Printing"
-        if SDCPStatus.EXPOSURE_TESTING in current_status:
+        if SDCPMachineStatus.EXPOSURE_TEST in current_status:
             return "Exposure Testing"
-        if SDCPStatus.DEVICES_TESTING in current_status:
+        if SDCPMachineStatus.DEVICE_TEST in current_status:
             return "Calibrating"
-        if SDCPStatus.TRANSFERRING in current_status:
+        if SDCPMachineStatus.FILE_TRANSFER in current_status:
             return "Uploading File"
 
         return "Unknown"
